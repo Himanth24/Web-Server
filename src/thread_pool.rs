bb6c2c4c2cfa4511df5544b98856a2cb35d01bd8
Himanth24 <himanthgodari@gmail.com>
2025-04-24 11:02:31 +0530

    adding test cases.

--- src/thread_pool.rs
+++ src/thread_pool.rs
@@ -49,3 +49,68 @@ impl Worker {
         Worker{ id, thread }
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::sync::{Arc, Mutex};
+    use std::thread;
+    use std::time::Duration;
+
+    #[test]
+    fn test_single_job_execution() {
+        let pool = ThreadPool::new(2);
+        let flag = Arc::new(Mutex::new(false));
+
+        let flag_clone = Arc::clone(&flag);
+        pool.execute(move || {
+            let mut flag = flag_clone.lock().unwrap();
+            *flag = true;
+        });
+
+        // Wait a bit to make sure thread runs
+        thread::sleep(Duration::from_millis(50));
+
+        assert_eq!(*flag.lock().unwrap(), true);
+    }
+
+    #[test]
+    fn test_multiple_jobs() {
+        let pool = ThreadPool::new(4);
+        let results = Arc::new(Mutex::new(Vec::new()));
+
+        for i in 0..5 {
+            let results_clone = Arc::clone(&results);
+            pool.execute(move || {
+                let mut vec = results_clone.lock().unwrap();
+                vec.push(i);
+            });
+        }
+
+        thread::sleep(Duration::from_millis(100));
+
+        let results = results.lock().unwrap();
+        assert_eq!(results.len(), 5);
+        for i in 0..5 {
+            assert!(results.contains(&i));
+        }
+    }
+
+    #[test]
+    fn test_jobs_run_concurrently() {
+        let pool = ThreadPool::new(2);
+        let start_time = std::time::Instant::now();
+
+        for _ in 0..2 {
+            pool.execute(|| {
+                thread::sleep(Duration::from_millis(100));
+            });
+        }
+
+        // If run sequentially, this would take ~200ms. We check it finishes sooner.
+        thread::sleep(Duration::from_millis(150));
+        let elapsed = start_time.elapsed();
+        assert!(elapsed < Duration::from_millis(200));
+    }
+}
